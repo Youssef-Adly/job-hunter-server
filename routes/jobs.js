1e8fe2a0c3bbb312d63eb8801166263aa7564d6f
Gameel Allam <fadyallam8@gmail.com>
2024-03-17 03:44:27 +0200

    -Update Job routes Permissions.

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const jobController = require("../controllers/jobs");
-const adminPermissions = require("../middlewares/admin.permissions");
 const companyPermissions = require("../middlewares/company.permissions");
 
 router.get("/", jobController.getAllJobs);
 router.get("/:id", jobController.getJobById);
 router.get("/company/:company", jobController.getJobByCompany);
 router.post("/", companyPermissions, jobController.createJob);
-router.put("/:id", adminPermissions, jobController.updateJob);
-router.patch("/:id", adminPermissions, jobController.patchJob);
+router.put("/:id", companyPermissions, jobController.updateJob);
+router.patch("/:id", companyPermissions, jobController.patchJob);
 router.delete("/:id", companyPermissions, jobController.deleteJob);
 
 module.exports = router;
